3f0b3f18061fcddbe4bc61f24349b138ba7fcbdc
Bakry200 <bakryalfadul@gmail.com>
2024-02-06 11:29:15 -0800

    so

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,8 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 
 	if (!head || !*head)
-		 return (-1);
-
+		return (-1);
 	if (!index)
 	{
 		node = *head;
