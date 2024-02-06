4ca6c156cdbfe5d69b1950fa8b33f052ce66a869
Bakry200 <bakryalfadul@gmail.com>
2024-02-05 23:55:44 -0800

    gg

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 /**
- * print_listint - prints a linked listes
+ * listint_len - prints a linked listes
  * @h: pointer to firest node
  * Return: size of list
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t i = 0;
+
 	while (h)
 	{
 		h = h->next;
