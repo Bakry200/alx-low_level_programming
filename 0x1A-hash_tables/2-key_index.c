9bf8637b33a8c780ae05ba005286b76d8cbd5ff6
Bakry200 <bakryalfadul@gmail.com>
2024-04-12 15:49:14 -0700

    edit

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,6 +5,7 @@
  * @size: index for the key
  * Return: index for the key
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size){
+{
+	 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	return (hash_djb2(key) % size);
 }
