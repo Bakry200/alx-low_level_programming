1399e0c91d679b5509c476b73ff9ff75faf22368
Bakry200 <bakryalfadul@gmail.com>
2024-04-12 15:54:40 -0700

    edit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -7,7 +7,7 @@
  */
 hash_node_t *make_hash_node(const char *key, const char *value)
 {
-	hash_node_t *node;
+	 hash_node_t *node;
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (NULL);
@@ -36,9 +36,9 @@ hash_node_t *make_hash_node(const char *key, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
+	 unsigned long int index;
 	hash_node_t *hash_node, *tmp;
-	char *new_value;
+	 char *new_value;
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 			key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
