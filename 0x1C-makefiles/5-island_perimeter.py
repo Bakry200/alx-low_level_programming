ed01143bb059b4815a0bdf425044ea30ae2fe333
Bakry200 <bakryalfadul@gmail.com>
2024-05-29 13:08:28 -0700

    go

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module contains the function island_perimeter which calculates
+the perimeter of an island in a given grid.
+"""
+def island_perimeter(grid):
+    """Calculate the perimeter of the island described in the grid"""
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                # Check top
+                if r == 0 or grid[r-1][c] == 0:
+                    perimeter += 1
+                # Check bottom
+                if r == rows-1 or grid[r+1][c] == 0:
+                    perimeter += 1
+                # Check left
+                if c == 0 or grid[r][c-1] == 0:
+                    perimeter += 1
+                # Check right
+                if c == cols-1 or grid[r][c+1] == 0:
+                    perimeter += 1
+    return perimeter
